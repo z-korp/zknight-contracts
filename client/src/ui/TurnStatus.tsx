10ff86f19fc9166c0fc7e972dda5333a0f136169
bal7hazar <bal7hazar@proton.me>
2023-10-11 13:54:32 +0200

    🔥 Remove client code

--- client/src/ui/TurnStatus.tsx
+++
@@ -1,9 +0,0 @@
-type Props = {
-  isProcessing: boolean;
-};
-
-const TurnStatus = ({ isProcessing }: Props) => {
-  return <div className="w-50">{isProcessing ? <p>Processing turn...</p> : <p>Your turn!</p>}</div>;
-};
-
-export default TurnStatus;
